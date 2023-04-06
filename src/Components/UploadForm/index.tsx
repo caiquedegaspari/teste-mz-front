8689e353fe79df10fa7f159637a8e0dd3fcb0cfb
caiquedegaspari <caique.degaspare@gmail.com>
2023-04-05 22:21:07 -0300

    feat: add uploadForm

--- src/Components/UploadForm/index.tsx
+++ src/Components/UploadForm/index.tsx
@@ -1,6 +1,37 @@
+import { toast } from "react-toastify"
+import { uploadSalesFile } from "../../services/uploadSalesFile"
+import { useState } from "react"
+import './form.css'
 
 export function UploadForm() {
+  
+  const [file, setFile] = useState<File | null>()
+
+  const handleChange = async (evt: React.ChangeEvent<HTMLInputElement>) => {
+    if (evt.target.files) {
+      const files: FileList = evt.target.files
+
+      if (files.length > 0) {
+        const file = files[0]
+        setFile(file)
+      }
+    }
+  }
+
+  const submit = async (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault()
+    if (!file) return toast.error('Adicione um arquivo')
+    await uploadSalesFile(file)
+    setFile(null)
+  }
+  
   return (
-    <h2>Upload Form component</h2>
+      <div className="container">
+        <form className="form" onSubmit={submit}>
+          <label htmlFor="salesFile">Arquivo do registro de vendas:</label>
+          <input type="file" name="salesFile" accept=".txt" onChange={handleChange}/> <br /><br />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
   )
 }
