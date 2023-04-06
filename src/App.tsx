6b6a1fc0b227524e51294e50c9209cd9e623989b
caiquedegaspari <caique.degaspare@gmail.com>
2023-04-05 21:01:29 -0300

    feat: config react-toastfy

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
     <div>
      <h1> Hello World</h1>
+     <ToastContainer />
     </div>
   );
 }
