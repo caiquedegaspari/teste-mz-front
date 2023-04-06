74a3ae4d3359f3e086e87b621da48db20f540e66
caiquedegaspari <caique.degaspare@gmail.com>
2023-04-06 20:45:20 -0300

    feat: add home logic to render components

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import { ListSales } from "../Components/ListSales";
 import { UploadForm } from "../Components/UploadForm";
+import './Home.css'
+
+type Components = 'upload' | 'listSales'
 
 export function Home() {
+  const [toggleComponent, setToggleComponent] = useState<Components>('upload')
+
+  const renderComponent = (componentType: Components) => {
+    switch (componentType) {
+      case 'upload':
+        return <UploadForm /> 
+      case 'listSales':
+        return <ListSales />
+      default: 
+        return <UploadForm />
+    }
+  }
+
   return (
     <>
-      <h1>
-        Home component
-      </h1>
-      <UploadForm />
-      <ListSales />
+      <button onClick={() => setToggleComponent('upload')}>Upload de arquivo de vendas</button>
+      <button onClick={() => setToggleComponent('listSales')}>Lista de vendas</button>
+
+      {
+        renderComponent(toggleComponent)
+      }
     </>
   )
 }
