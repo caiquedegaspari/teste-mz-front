c67b9514720c3f81890151e84a3b90fbdbf1f572
caiquedegaspari <caique.degaspare@gmail.com>
2023-04-06 23:53:33 -0300

    feat: finish listSales api integration

--- src/Components/ListSales/index.tsx
+++ src/Components/ListSales/index.tsx
@@ -2,76 +2,24 @@ import { format } from 'date-fns'
 
 import { SalesListFromApiDTO } from '../../services/dtos/SellersList.dto'
 import './listSales.css'
+import { useEffect, useState } from 'react'
+import { listSales } from '../../services/listSales'
 
 export function ListSales() {
   
   const headers = ['Tipo', 'Data', 'Produto', 'Valor', 'Vendedor']
-  const data: SalesListFromApiDTO[] = [
-    {
-      sellerTable: [
-        {
-          type: 'Venda produtor',
-          date: new Date('2021-12-03T14:46:02.000Z'),
-          product: 'DOMINANDO INVESTIMENTOS',
-          value: 50000,
-          sellerName: 'MARIA CANDIDA'
-        },
-        {
-          type: 'Venda produtor',
-          date: new Date('2022-01-22T11:59:13.000Z'),
-          product: 'DOMINANDO INVESTIMENTOS',
-          value: 50000,
-          sellerName: 'MARIA CANDIDA'
-        },
-      ],
-      total: 100000
-    },
-    {
-      sellerTable: [
-        {
-          type: 'Venda filiado',
-          date: new Date('2022-01-22T11:59:13.000Z'),
-          product: 'CURSO JAVASCRIPT',
-          value: 10000,
-          sellerName: 'ALUISIO AZEVEDO'
-        },
-        {
-          type: 'Venda filiado',
-          date: new Date('2022-01-22T11:59:13.000Z'),
-          product: 'CURSO JAVASCRIPT',
-          value: 10000,
-          sellerName: 'ALUISIO AZEVEDO'
-        },
-      ],
-      total: 20000,
-    },
-    {
-      sellerTable: [
-        {
-          type: 'Venda produtor',
-          date: new Date('2022-01-22T11:59:13.000Z'),
-          product: 'CURSO JAVASCRIPT',
-          value: 10000,
-          sellerName: 'JOANA SILVA'
-        },
-        {
-          type: 'Comissão paga',
-          date: new Date('2022-01-22T11:59:13.000Z'),
-          product: 'CURSO JAVASCRIPT',
-          value: 10000,
-          sellerName: 'JOANA SILVA'
-        }
-      ],
-      total: 20000
-    }
-  ]
+  const [sales, setSales] = useState<SalesListFromApiDTO[] | undefined>([])
+  
+  useEffect(() => {
+    listSales().then((res) => setSales(res))
+  }, [])
 
   return (
     <>
       <h2>Lista de vendas por vendedor/afiliado</h2>
       <div className='tableContainer'>
         {
-          data.map((tables, dataKey) => (
+          sales?.map((tables, dataKey) => (
             <table key={dataKey}>
               <tr>
                 {headers.map((item, index) => (
@@ -82,9 +30,9 @@ export function ListSales() {
                 tables.sellerTable.map((sales, idx) => (
                     <tr key={idx}>
                       <td>{sales.type}</td>
-                      <td>{format(sales?.date, 'dd/MM/yyyy')}</td>
+                      <td>{format(new Date(sales?.date), 'dd/MM/yyyy')}</td>
                       <td>{sales.product}</td>
-                      <td>{sales.value}</td>
+                      <td>{toBrCurrency(sales.value / 100)}</td>
                       <td>{sales.sellerName}</td>
                     </tr>
                   )
